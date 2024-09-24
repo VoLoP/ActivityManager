d1864efeb66449a75b3cd202661ff2f9f2341cda
Simone <bativolpet1@gmail.com>
2024-09-24 11:43:08 +0200

    Added the foundation for the project structure
    Added Activity class with the properties
    Created ActivityRepo and IActivityRepo
    Added ActivityController

---
+++ ActivityManager.DAL/Repos/ActivityRepo.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ActivityManager.DAL.Repos
+{
+    public class ActivityRepo
+    {
+    }
+}
