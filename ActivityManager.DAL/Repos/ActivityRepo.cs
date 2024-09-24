5b0f8f417798dd86d95a1ff15953efee5729918d
Simone <bativolpet1@gmail.com>
2024-09-24 11:48:47 +0200

    Implemented ActivityRepo and IActivityRepo
    Implemented ActivityRepo.cs  for managing Activity objects with methods for CRUD operations. Introduced a private list _activities to store activities. Updated IActivityRepo.cs to define an interface IActivityRepo with the same methods. Adjusted usings

--- ActivityManager.DAL/Repos/ActivityRepo.cs
+++ ActivityManager.DAL/Repos/ActivityRepo.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using ActivityManager.DAL.Data;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +9,41 @@ namespace ActivityManager.DAL.Repos
 {
     public class ActivityRepo
     {
+        private readonly List<Activity> _activities = new List<Activity>();
+
+        public IEnumerable<Activity> GetAll()
+        {
+            return _activities;
+        }
+
+        public Activity GetById(int id)
+        {
+            return _activities.SingleOrDefault(a => a.Id == id);
+        }
+
+        public void Add(Activity activity)
+        {
+            _activities.Add(activity);
+        }
+
+        public void Update(Activity activity)
+        {
+            var existingActivity = GetById(activity.Id);
+            if (existingActivity != null)
+            {
+                existingActivity.Name = activity.Name;
+                existingActivity.Date = activity.Date;
+                existingActivity.Description = activity.Description;
+            }
+        }
+
+        public void Delete(int id)
+        {
+            var activity = GetById(id);
+            if (activity != null)
+            {
+                _activities.Remove(activity);
+            }
+        }
     }
 }
--- ActivityManager.DAL/Repos/IRepos/IActivityRepo.cs
+++ ActivityManager.DAL/Repos/IRepos/IActivityRepo.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using ActivityManager.DAL.Data;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +9,10 @@ namespace ActivityManager.DAL.Repos.IRepos
 {
     public interface IActivityRepo
     {
+        IEnumerable<Activity> GetAll();
+        Activity GetById(int id);
+        void Add(Activity activity);
+        void Update(Activity activity);
+        void Delete(int id);
     }
 }
