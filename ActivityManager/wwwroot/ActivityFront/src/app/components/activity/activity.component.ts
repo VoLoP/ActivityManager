374ffddc39f6fe28560b18e6e52bddba6a39e2de
Simone <bativolpet1@gmail.com>
2024-09-27 15:50:58 +0200

    Implemented activity.component.ts

--- ActivityManager/wwwroot/ActivityFront/src/app/components/activity/activity.component.ts
+++ ActivityManager/wwwroot/ActivityFront/src/app/components/activity/activity.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivityService } from '../../services/activity.service';
+import { Activity } from '../../models/activity.model';
 
 @Component({
   selector: 'app-activity',
@@ -7,6 +9,33 @@ import { Component } from '@angular/core';
   templateUrl: './activity.component.html',
   styleUrl: './activity.component.scss'
 })
-export class ActivityComponent {
+export class ActivityComponent implements OnInit {
 
+  activities: Activity[] = [];
+  newActivity: Activity = { id: 0, name: '', date: new Date(), description: '' };
+
+  constructor(private activityService: ActivityService) {}
+
+  ngOnInit(): void {
+    this.loadActivities();
+  }
+
+  loadActivities(): void {
+    this.activityService.getAll().subscribe(data => {
+      this.activities = data;
+    });
+  }
+
+  addActivity(): void {
+    this.activityService.create(this.newActivity).subscribe(() => {
+      this.loadActivities();
+      this.newActivity = { id: 0, name: '', date: new Date(), description: '' };
+    });
+  }
+
+  deleteActivity(id: number): void {
+    this.activityService.delete(id).subscribe(() => {
+      this.loadActivities();
+    });
+  }
 }
